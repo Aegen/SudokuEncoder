5c8fa8bf0a5f5d70669e320b7e9d86e6a51109f6
Aegen <kyuzo101@gmail.com>
2021-11-02 00:32:56 -0500

    Added Utilities

---
+++ SudokuParser/Controllers/Utilities/BinaryConversionUtil.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace SudokuService.Controllers.Utilities
+{
+    public class BinaryConversionUtil
+    {
+        private BinaryConversionUtil() { }
+
+        public static byte[] GetBytes(string bitString)
+        {
+            return Enumerable.Range(0, bitString.Length / 8).
+                Select(pos => Convert.ToByte(
+                    bitString.Substring(pos * 8, 8),
+                    2)
+                ).ToArray();
+        }
+    }
+}
