e8c0620112d701849fa980dba73f5933029f96e3
Aegen <kyuzo101@gmail.com>
2021-11-11 14:41:00 -0600

    Changed project folder name to match the project name

--- SudokuService/Services/EncoderService.cs
+++ SudokuService/Services/EncoderService.cs
@@ -12,33 +12,27 @@ namespace SudokuService.Services
         {
             var tokens = TokenizeGrid(grid);
 
-            var longestJumpBits = GetLongestJumpBits(tokens);
+            var maxJumpBitsNeeded = GetLongestJumpBits(tokens);
             var binaryStringBuilder = new StringBuilder();
 
             // Add max jump section
-            binaryStringBuilder.Append(BinaryUtils.LeftPadToSize(BinaryUtils.IntToBinary(longestJumpBits), 3));
+            binaryStringBuilder.Append(BinaryUtils.LeftPadToSize(BinaryUtils.IntToBinary(maxJumpBitsNeeded), 3));
 
-            // Add chain bit for first value
-            if (tokens[0].Type == CellTypeEnum.EMPTY)
-            {
-                binaryStringBuilder.Append('0');
-            }
-            else
-            {
-                binaryStringBuilder.Append('1');
-            }
+            // Add initial bit for whether the first cell has a value in it
+            var initialValueBit = (tokens.First().Type == CellTypeEnum.EMPTY) ? '0' : '1';
+            binaryStringBuilder.Append(initialValueBit);
 
-            var tokenBinaryStrings = GetTokenBinaryStrings(tokens, longestJumpBits);
+            var tokenBinaryStrings = GetTokenBinaryStrings(tokens, maxJumpBitsNeeded);
 
             tokenBinaryStrings.ForEach(tokenBinaryString => binaryStringBuilder.Append(tokenBinaryString));
 
             return binaryStringBuilder.ToString();
         }
 
-        private static List<String> GetTokenBinaryStrings(List<Token> tokens, int longestJumpBits)
+        private static List<String> GetTokenBinaryStrings(List<Token> tokens, int maxJumpBits)
         {
             var sudokuGrid = new SudokuGrid();
-            return tokens.Select(token => TokenToBitString(token, longestJumpBits, ref sudokuGrid)).ToList();
+            return tokens.Select(token => TokenToBinary(token, maxJumpBits, ref sudokuGrid)).ToList();
         }
 
         private static int FindValueIndex(List<int> possibleValue, int valueToFind)
@@ -51,10 +45,10 @@ namespace SudokuService.Services
                 }
             }
 
-            return -1;
+            return -1; //TODO: Maybe change this to an exception, happy pathing right now
         }
 
-        private static String TokenToBitString(Token token, int longestJumpBits, ref SudokuGrid sudokuGrid)
+        private static String TokenToBinary(Token token, int maxJumpBits, ref SudokuGrid sudokuGrid)
         {
             var bitString = new StringBuilder();
             if (token.Type == CellTypeEnum.VALUE)
@@ -77,7 +71,7 @@ namespace SudokuService.Services
             else if (token.Type == CellTypeEnum.EMPTY && token.StartIndex != 80)
             {
                 var rawBits = BinaryUtils.IntToBinary(token.Length - 1);
-                var neededBitCount = GetJumpBitsPerSchedule(token.StartIndex, longestJumpBits);
+                var neededBitCount = GetJumpBitsPerSchedule(token.StartIndex, maxJumpBits);
                 var valueBits = BinaryUtils.LeftPadToSize(rawBits, neededBitCount);
                 bitString.Append(valueBits);
             }
@@ -88,13 +82,13 @@ namespace SudokuService.Services
         private static List<Token> TokenizeGrid(List<int> grid)
         {
             if (grid == null || grid.Count == 0)
-                throw new ArgumentException("The provided grid is either null or empty");
+                throw new ArgumentException("The provided grid is either null or empty", nameof(grid));
 
-            var islands = new List<Token> { CreateIslandToken(grid[0], 0) };
+            var tokens = new List<Token> { CreateIslandToken(grid[0], 0) };
 
             for (var index = 1; index < grid.Count; index++)
             {
-                var newestIsland = islands.Last();
+                var newestIsland = tokens.Last();
 
                 if (newestIsland.Type == GetCellType(grid[index]))
                 {
@@ -103,11 +97,11 @@ namespace SudokuService.Services
                 }
                 else
                 {
-                    islands.Add(CreateIslandToken(grid[index], index));
+                    tokens.Add(CreateIslandToken(grid[index], index));
                 }
             }
 
-            return islands;
+            return tokens;
         }
 
         private static Token CreateIslandToken(int cellValue, int index)
@@ -128,10 +122,10 @@ namespace SudokuService.Services
                 .Select(token => BinaryUtils.NumBitsToStore(token.Length))
                 .Max();
 
-        public static int GetJumpBitsPerSchedule(int address, int longestJumpBits)
+        public static int GetJumpBitsPerSchedule(int address, int maxJumpBits)
         {
             var indexBits = BinaryUtils.NumBitsToStore(81 - address);
-            return (indexBits < longestJumpBits) ? indexBits : longestJumpBits;
+            return (indexBits < maxJumpBits) ? indexBits : maxJumpBits;
         }
     }
 }
